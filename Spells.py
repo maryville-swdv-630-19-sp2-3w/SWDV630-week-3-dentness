d1868104156d21d7f47df033671385d516b40871
Joe Dent <14243877+dentness@users.noreply.github.com>
2019-03-31 13:39:51 -0500

    Altered to address question changes.

--- Spells.py
+++ Spells.py
@@ -1,3 +1,7 @@
+# SWDV-630-3W 2019/SP2
+# Joe Dent
+# Week 3
+
 class Spell:
     def __init__(self, incantation, name):
         self.name = name
@@ -17,6 +21,9 @@ class Accio(Spell):
     def __init__(self):
         Spell.__init__(self, 'Accio', 'Summoning Charm')
 
+    def get_description(self):
+        return 'This charm summons an object to the caster, potentially over a significant distance'
+
 
 class Confundo(Spell):
     def __init__(self):
@@ -26,11 +33,11 @@ class Confundo(Spell):
         return 'Causes the victim to become confused and befuddled.'
 
 
-def study_spell(incantation):
-    print(incantation)
+def study_spell(spell):
+    print(spell)
 
-
-spell = Accio()
-spell.execute()
-study_spell(spell)
+# Changed spell => a_spell, to keep python from being grumpy about study_spell(spell), as spell "shadows name spell from outer scope"
+a_spell = Accio()
+a_spell.execute()
+study_spell(a_spell)
 study_spell(Confundo())
